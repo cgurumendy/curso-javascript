27767f21884e6531d4bda20f075cb892c45dd51c
Cristian G <cristiangurumendy@gmail.com>
2025-05-26 20:49:18 +0200

    Begginer exercises + Duda push GitHub

---
+++ nivel-basico/03-beginner-exercises.js
@@ -0,0 +1,78 @@
+// 1. Escribe un comentario en una línea
+
+// Esto es un comentario de una línea
+
+// 2. Escribe un comentario en varias líneas
+
+/*
+Esto es un comentario
+de varias lineas
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+let nombre = "Cristian"
+let peso = 100
+let altura = 1.80
+let trabajador = true
+let dineroCuenta
+let titulos = null
+let milisegundosVividos = BigInt(79246422287384728273828372341)
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(nombre)
+console.log(peso)
+console.log(altura)
+console.log(trabajador)
+// console.log(dineroCuenta)
+console.log(titulos)
+console.log(milisegundosVividos)
+
+// 5. Imprime por consola el tipo de todas las variables
+
+console.log(typeof(nombre))
+console.log(typeof(peso))
+console.log(typeof(altura))
+console.log(typeof(trabajador))
+console.log(typeof(dineroCuenta))
+console.log(typeof(titulos))
+console.log(typeof(milisegundosVividos))
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+nombre = "Adrian"
+peso = 80
+altura = 1.79
+trabajador = false
+titulos = null
+milisegundosVividos = BigInt(0000000000000000000000000000000000000000)
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+nombre = 98
+peso = "ochenta kilos"
+altura = true
+trabajador = "Gurumendy"
+titulos = BigInt(11111111111111111111111111111111111111111111111111111)
+milisegundosVividos = null
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+const nombreConst = "Pedro"
+const pesoConst = 98
+const alturaConst = 1.81
+const trabajadorConst = true
+const titulosConst = null
+const milisegundosVividosConst = BigInt(999999999999999999999999999999)
+
+// 9. A continuación, modifica los valores de las constantes
+
+// nombreConst = "Javier"
+// pesoConst = 89
+// alturaConst = 1.90
+// trabajadorConst = false
+// titulosConst = 3
+// milisegundosVividosConst = BigInt(00000000000000000000000000000000)
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
--- readme.md
+++ readme.md
@@ -1,3 +1,21 @@
 # Curso JavaScript de MoureDev
 
 - Mi intención es escribir código y apuntes del curso y subirlo a GitHub para acceder a ellos desde cualquier dispotivo.
+
+- Duda push cambios de repositorio local a remoto (GitHub):
+
+<img src="./img/WhatsApp%20Image%202025-05-26%20at%2020.36.21.jpeg">
+
+<img src="./img/WhatsApp Image 2025-05-26 at 20.36.21 (1).jpeg">
+
+<img src="./img/WhatsApp Image 2025-05-26 at 20.36.21 (2).jpeg">
+
+<img src="./img/WhatsApp Image 2025-05-26 at 20.36.21 (3).jpeg">
+
+<img src="./img/WhatsApp Image 2025-05-26 at 20.36.21 (4).jpeg">
+
+<img src="./img/WhatsApp Image 2025-05-26 at 20.36.21 (5).jpeg">
+
+<img src="./img/WhatsApp Image 2025-05-26 at 20.36.21 (6).jpeg">
+
+<img src="./img/WhatsApp Image 2025-05-26 at 20.36.21 (7).jpeg">
